a3729a3e3627a410f2c49a630de76e2ed735a2de
Mateoes <mateoescudero10@gmail.com>
2025-08-26 21:43:47 -0500

    Feature añade departamento

---
+++ AppNomina/models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppNomina.Models
+{
+    [Table("Departments")]
+    public class Department
+    {
+        [Key]
+        [StringLength(4, ErrorMessage = "El código del departamento no puede tener más de 4 caracteres.")]
+        [Display(Name = "Código")]
+        public string DeptNo { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(40, ErrorMessage = "El nombre no puede superar los 40 caracteres.")]
+        [Display(Name = "Nombre del Departamento")]
+        public string DeptName { get; set; }
+    }
+}
---
+++ AppNomina/models/EmpleadosReutilizacionContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace AppNomina.Models
+{
+    public class EmpleadosReutilizacionContext : DbContext
+    {
+        // Usa la cadena de conexión "Cnn" definida en Web.config
+        public EmpleadosReutilizacionContext() : base("name=Cnn")
+        {
+        }
+
+        // Aquí defines las tablas que vas a mapear
+        public DbSet<Department> Departments { get; set; }
+        // Más adelante puedes añadir Employees, Salaries, etc.
+    }
+}
