dbf43a04f0bb7e22ff9f2bd91f02314fcb47527d
Andy Guallichico <andy.guallichico@ister.edu.ec>
2025-08-26 23:03:58 -0500

    fix: Departaments

--- AppNomina/models/Department.cs
+++ AppNomina/models/Department.cs
@@ -3,17 +3,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AppNomina.Models
 {
-    [Table("Departments")]
     public class Department
     {
-        [Key]
-        [StringLength(4, ErrorMessage = "El código del departamento no puede tener más de 4 caracteres.")]
-        [Display(Name = "Código")]
-        public string DeptNo { get; set; }
-
-        [Required(ErrorMessage = "El nombre es obligatorio.")]
-        [StringLength(40, ErrorMessage = "El nombre no puede superar los 40 caracteres.")]
-        [Display(Name = "Nombre del Departamento")]
-        public string DeptName { get; set; }
+        public int DeptNo { get; set; }  // Número de departamento
+        public string DeptName { get; set; }  // Nombre del departamento
     }
 }
